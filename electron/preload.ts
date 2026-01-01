87031fe0eb14a703f1d65d843eade1e2214066f5
dianyuluo <1739556970@qq.com>
2026-01-02 00:48:32 +0800

    修改函数名

--- electron/preload.ts
+++ electron/preload.ts
@@ -2,5 +2,5 @@ import { contextBridge, ipcRenderer } from 'electron';
 
 // 在这里可以暴露安全的 API 给渲染进程
 contextBridge.exposeInMainWorld('electronAPI', {
-  createAndInsertTime111: () => ipcRenderer.invoke('create-and-insert-time'),
+  createAndInsertTime: () => ipcRenderer.invoke('create-and-insert-time'),
 });
