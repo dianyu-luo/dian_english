e496bca29b78dfcfccdc64032a1ab6ba948b0a85
dianyuluo <1739556970@qq.com>
2026-01-02 00:34:06 +0800

    配置项目有些不应该存在，因为他是编译部分

--- dist-electron/preload.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const electron_1 = require("electron");
-// 在这里可以暴露安全的 API 给渲染进程
-electron_1.contextBridge.exposeInMainWorld('electronAPI', {
-    createAndInsertTime: () => electron_1.ipcRenderer.invoke('create-and-insert-time'),
-});
